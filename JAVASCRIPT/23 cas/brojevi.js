3896a41b4cb7fa0e3fb4f23d6e8e52799204a5f3
alenmuslic <muslicalen15@gmail.com>
2022-12-13 19:51:58 +0100

    ,

---
+++ JAVASCRIPT/23 cas/brojevi.js
@@ -0,0 +1,42 @@
+// BROJEVI
+const noviBroj = 555e-5;
+console.log(noviBroj);
+
+//Sto se tice Js SABIRANJA DECImalnim brojevima nije bas najbolji kalkulato
+
+// + omrator
+console.log("10" + 10); //"1010"
+
+console.log("10" - 2); //8
+
+//NaN
+//NaN je JS rezervisana rec koja ukazuje da broj nije korektan.
+//NaN je tipa number!!!!!!!!!!!!!!!!!!!!!!!
+//Za proveru da li je neka vrednost
+
+// Infinity ili -infinity je vrednost koju cemo da doijemo kada rezultat premasi najveci broj u JavaScriptu (bude veoma mali broj)
+const c = -10;
+const a = 25;
+const b = 23;
+console.log(a / b);
+console.log(c / b);
+console.log(typeof Infinity);
+
+rgb(0, 255, 123);
+//Ako su brojevi napisani prefiksom  0xFF ce biti 255
+const r = 0xff;
+
+//12=1*16 +2*1
+
+// Napraviti funkciju koja pretvara Kilometre u metre b) ince u inche u cm
+// function km()
+// const km=m/1000
+// const in=cm/2.54
+// console.log(m)
+// console.log (cm)
+
+function nesto(km) {
+  let km = m / 1000;
+  return km;
+}
+console.log(nesto(200));
