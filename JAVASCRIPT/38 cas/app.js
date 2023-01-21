50e0c427a27807f44720ff284059edd5742e39bf
alenmuslic <muslicalen15@gmail.com>
2023-01-21 19:07:09 +0100

    sdasdasdsa

---
+++ JAVASCRIPT/38 cas/app.js
@@ -0,0 +1,19 @@
+//napraviti funkciju  koja brise duplikate iz niza i ignorisati case sensivity
+let niz = ["Alen", "alen", "Amin", "amin"];
+
+const second = (arr) => {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.slice(0, i).includes(arr[i])) {
+      continue;
+    }
+  }
+};
+
+let boje = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellw"];
+o = ["st", "nd", "rd", "th"];
+
+const first = (arr1) => {
+  const brojac = [];
+  for (let i = 0; i < arr.length; i++) {}
+};
