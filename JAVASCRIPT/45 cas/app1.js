0f585d70c729d976b9de68309681a760a94c2e00
alenmuslic <muslicalen15@gmail.com>
2023-02-09 12:48:25 +0100

    protiv coveka

--- JAVASCRIPT/45 cas/app1.js
+++ JAVASCRIPT/45 cas/app1.js
@@ -1,21 +1,38 @@
-// Jos jedan nacin pravljenja objekta:
+// // Jos jedan nacin pravljenja objekta:
 
-const objectFunctions = {
-  incrementScore: function () {
-    this.score += 1;
+// const objectFunctions = {
+//   incrementScore: function () {
+//     this.score += 8;
+//   },
+// };
+
+// function createUser(firstName, lastName, score) {
+//   const newUser = Object.create(objectFunctions);
+//   newUser.firstName = firstName;
+//   newUser.lastName = lastName;
+//   newUser.score = score;
+//   return newUser;
+// }
+
+// const tarik = createUser("Tarik", "Ibrovic", 16);
+// console.log(tarik);
+
+// tarik.incrementScore();
+// console.log(tarik);
+
+const objekatNeki = {
+  dodatniBroj: function () {
+    this.godine += 1;
   },
 };
-
-function createUser(firstName, lastName, score) {
-  const newUser = Object.create(objectFunctions);
-  newUser.firstName = firstName;
-  newUser.lastName = lastName;
-  newUser.score = score;
-  return newUser;
+function napraviLika(ime, prezime, godine) {
+  const noviLik = Object.create(objekatNeki);
+  noviLik.ime = ime;
+  noviLik.prezime = prezime;
+  noviLik.godine = godine;
+  return noviLik;
 }
-
-const tarik = createUser("Tarik", "Ibrovic", 16);
-console.log(tarik);
-
-tarik.incrementScore();
-console.log(tarik);
+const alen = napraviLika("Alen", "Muslic", 17);
+console.log(alen);
+alen.dodatniBroj();
+console.log(alen);
