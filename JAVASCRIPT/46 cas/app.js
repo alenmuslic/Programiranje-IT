b4a8cf5698a81944497ed21aacbc68821c75d05c
alenmuslic <muslicalen15@gmail.com>
2023-02-09 20:58:22 +0100

    43. godina

---
+++ JAVASCRIPT/46 cas/app.js
@@ -0,0 +1,7 @@
+//1. Iz 2 poslata niza vratiti 3. niz koji ce sadrzti one elemente iz oba niza koji nisu sadrzani u onom drugom nizu
+
+// const difference= (arr1,arr2)=>{
+//     const helper1=niz1.filter((element) => !arr2.includes((element))
+//     const helper2=niz1.filter((element) => !arr1.includes((element))
+
+// }
