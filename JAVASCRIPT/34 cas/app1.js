54db75656b0891cbb352bd8979d805dc7ce3b7bc
alenmuslic <muslicalen15@gmail.com>
2023-02-15 15:19:59 +0100

    nizovi vezbanje

--- JAVASCRIPT/34 cas/app1.js
+++ JAVASCRIPT/34 cas/app1.js
@@ -1,15 +1,29 @@
-// callback funkcija //
+// callback funkcije //
+
+// callback funkcija predstavlja funkciju koju saljemo kao argument neke druge funkcije.
+// Sam argument ne treba da bude izvrsavanje callback funkcije vec referenca na datu radnju.
+
 function glavnaFunkcija(func) {
-  console.log("Radnja unutar glavne funkcije");
-  func((par1 = 2), (par2 = 5));
+  console.log("Radnja unutar glavne funkcije.");
+  func();
   console.log("Kraj glavne funkcije.");
 }
 
-function callback(par1, par2) {
-  console.log(par1 ** par2);
+function callback() {
+  console.log("Radnja unutar callback funkcije.");
 }
+
 glavnaFunkcija(callback);
 
-//Cakback funkcija oredstavlja funkciju koju saljemo kao argument neke dtuge funkcije,
+// Napraviti glavnu funkciju unutar koje ce se desiti ispis sta ona predstavlja, a celu stvar ce da izvrsi callback funkcija. Callback funkcija ima 2 parametra, i ispisuje rezultat par1 stepenovan par2.
 
-// Napraviti glavnu funkciju unutar koje ce se desiti radnja ispis
+const mainFunction = (callBack2) => {
+  console.log("Prvi broj stepenovan drugim je:");
+  callBack2(2, 3);
+};
+
+const callBack2 = (par1, par2) => {
+  console.log(par1 ** par2);
+};
+
+mainFunction(callBack2);
