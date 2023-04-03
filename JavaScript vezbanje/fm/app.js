5268528e4d7637c23c584db9db260d01457403c9
alenmuslic <muslicalen15@gmail.com>
2023-04-03 22:58:24 +0200

    vs

---
+++ JavaScript vezbanje/fm/app.js
@@ -0,0 +1,8 @@
+const alen = ["alen", "muslic", "sadeta", "kurva"];
+
+const istina = alen.filter((lol) => lol.length > 3);
+console.log(istina);
+
+const brojevi = [1, 3, 4, 6, 8, 2];
+const veci = brojevi.map((x) => x + 2);
+console.log(veci);
